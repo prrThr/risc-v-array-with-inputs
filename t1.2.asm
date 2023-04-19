15e101e55a3719ac7a9937393dd4f8889a2cc639
prrThr <arthur.op9@gmail.com>
2023-04-19 17:30:28 -0300

    codigo em C++ adicionado

--- t1.2.asm
+++ t1.2.asm
@@ -1,10 +1,32 @@
 # Disciplina: Arquitetura e Organização de Processadores  
-# Atividade: Avaliação 01 – Programação em Linguagem de Montagem 
-# Exercício 03
-# Alunos: Arthur de Oliveira Pereira  
-#		  Henrik Gomes Baltazar  
+# Atividade: Avaliação 02 – Programação em Linguagem de Montagem 
+# Programa 02
+# Alunos: - Arthur de Oliveira Pereira  
+#		  - Henrik Gomes Baltazar  
 # -------------------------------------------------------------------# 
 # ----------- Código em C++ com a biblioteca std incluida -----------# 
+#	int Vetor_A[7] = {0};
+#	int tamanho = 0;
+#	int i;
+#  
+#	while (tamanho < 1 || tamanho > 8){
+#		cout << "Entre com o tamanho do vetor (max. = 8): ";
+#  		cin >> tamanho;
+#    
+#		if (tamanho < 1 || tamanho > 8)
+#    	cout << "Valor invalido.\n";
+#	}
+#
+#	for (i = 0; i < tamanho; i++){
+#		cout << "Vetor_A[" << i << "]: ";
+#    	cin >> Vetor_A[i];
+#  	}
+#
+#	cout << "Digite o indice do valor a ser impresso: ";
+#  	cin >> i;
+#
+#  	cout << "O elemento do vetor na posicao " << i << " possui o valor " << Vetor_A[i];
+
 
 	.data
 	.text
