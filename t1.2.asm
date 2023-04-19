0944a8c62c01aeded5b445e916bc74c81d654708
arthur <arthur.op9@gmail.com>
2023-04-19 14:41:55 -0300

    cabeçalho

---
+++ t1.2.asm
@@ -0,0 +1,11 @@
+# Disciplina: Arquitetura e Organização de Processadores  
+# Atividade: Avaliação 01 – Programação em Linguagem de Montagem 
+# Exercício 03
+# Alunos: Arthur de Oliveira Pereira  
+#		  Henrik Gomes Baltazar  
+# -------------------------------------------------------------------# 
+# ----------- Código em C++ com a biblioteca std incluida -----------# 
+
+	.data
+	.text
+main:
